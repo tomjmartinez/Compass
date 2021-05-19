d0273dab1a486e8771f933b61b272b4b901f7642
tomjmartinez <23747199+tomjmartinez@users.noreply.github.com>
2021-05-18 22:33:40 -0700

    add springbootmongo dependency, rm mysteryflag in GeoCache, add approved bool

--- Project2_Backend/src/main/java/com/example/Project2/Project2BackendApplication.java
+++ Project2_Backend/src/main/java/com/example/Project2/Project2BackendApplication.java
@@ -1,13 +1,32 @@
 package com.example.Project2;
 
+import com.example.Project2.models.GeoCache;
+import com.example.Project2.models.GeoCacheRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Project2BackendApplication {
 
+	private GeoCache geoCache;
+
+	@Autowired
+	public Project2BackendApplication(GeoCache geoCache) {
+		this.geoCache = geoCache;
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(Project2BackendApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner() {
+		return a -> {
+			GeoCache geoCache1 = GeoCacheRepo.findById(new ObjectId("")).orElse(null);
+		};
 	}
 
 }
---
+++ Project2_Backend/src/main/java/com/example/Project2/models/GeoCache.java
@@ -0,0 +1,23 @@
+package com.example.Project2.models;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Arrays;
+
+/**Class represents
+ * @author: Tomas J. Martinez
+ */
+@Document(collection="compass")
+public class GeoCache {
+    @Id
+    Object id;
+    float[] coordinates = new float[2];
+    String description;
+    String gifter;
+    String reviewer;
+    boolean approved;
+    String finder;
+    long timeLimit;
+
+}
