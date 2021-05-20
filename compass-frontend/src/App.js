408d1fc0ebdb60dc43b12229c6eb0f5597640996
Kevin <kdalt412@gmail.com>
2021-05-19 22:12:27 -0700

    Added Maps

--- compass-frontend/src/App.js
+++ compass-frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import CreateAccountComponent from "./components/CreateAccountComponent";
+import MapComponent from "./components/MapComponent";
 import "./App.css";
 
 
@@ -8,7 +9,7 @@ class App extends React.Component{
   render() {
     return(
         <div className={"App"}>
-          <CreateAccountComponent></CreateAccountComponent>
+          <MapComponent></MapComponent>
         </div>
     );
   }
---
+++ compass-frontend/src/components/MapComponent.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import { Map, GoogleApiWrapper, InfoWindow, Marker } from 'google-maps-react';
+
+import CurrentLocation from './CurrentLocationComponent';
+
+const mapStyles = {
+  disableDefaultUI: true,
+  position: 'absolute',
+  width: '85%',
+  height: '85%',
+  zoomControl: true,
+  streetViewControl: false
+};
+
+export class MapComponent extends Component {
+  state = {
+    showingInfoWindow: false,  // Hides or shows the InfoWindow
+    activeMarker: {},          // Shows the active marker upon click
+    selectedPlace: {}          // Shows the InfoWindow to the selected place upon a marker
+  };
+
+  onMarkerClick = (props, marker, e) =>
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+    });
+
+  onClose = props => {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      });
+    }
+  };
+
+  render() {
+    return (
+      <CurrentLocation
+        centerAroundCurrentLocation
+        google={this.props.google}
+      >
+        <Marker onClick={this.onMarkerClick} name={'Current Location'} />
+        <InfoWindow
+          marker={this.state.activeMarker}
+          visible={this.state.showingInfoWindow}
+          onClose={this.onClose}
+        >
+          <div>
+            <h4>{this.state.selectedPlace.name}</h4>
+          </div>
+        </InfoWindow>
+      </CurrentLocation>
+    );
+  }
+}
+
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyB1dfOSar0Tt4sm84JntjfLujmj8GE_u8I'
+})(MapComponent);
