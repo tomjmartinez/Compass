a1bc1917a9fa55d99974dd232d3f8084acdcef20
Kevin <kdalt412@gmail.com>
2021-05-19 17:16:10 -0700

    Small changes

--- Project2_Backend/src/main/java/com/example/Project2/models/UserRepo.java
+++ Project2_Backend/src/main/java/com/example/Project2/models/UserRepo.java
@@ -3,5 +3,5 @@ package com.example.Project2.models;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface UserRepo extends MongoRepository<GeoCache, ObjectId> {
+public interface UserRepo extends MongoRepository<User, ObjectId> {
 }
