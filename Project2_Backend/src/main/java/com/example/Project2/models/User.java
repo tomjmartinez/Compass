3fa0893a7db5579193f19f747bde5f648fd1b247
Tomas Jaziel Martinez <23747199+tomjmartinez@users.noreply.github.com>
2021-05-21 11:02:29 -0700

    testing code with jdk11 works

--- Project2_Backend/src/main/java/com/example/Project2/models/User.java
+++ Project2_Backend/src/main/java/com/example/Project2/models/User.java
@@ -1,9 +1,11 @@
 package com.example.Project2.models;
 
+import lombok.Data;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+@Data
 @Document(collection="users")
 public class User {
     @Id
