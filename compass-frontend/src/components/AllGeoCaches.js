641cd80cca7f448f70fd28a6b422e5c61c84eefc
Tomas Jaziel Martinez <23747199+tomjmartinez@users.noreply.github.com>
2021-05-25 12:21:12 -0700

    reusable functional component GeoCachesTable

--- compass-frontend/src/components/AllGeoCaches.js
+++ compass-frontend/src/components/AllGeoCaches.js
@@ -1,5 +1,6 @@
 import axios from "axios"
 import React, { Component } from "react"
+import GeoCachesTable from "./GeoCachesTable"
 import "../styles/geostyles.css"
 
 //const MyGeoCaches = () => {}
@@ -23,41 +24,7 @@ class AllGeoCaches extends Component{
     render(){
         const {allGeoCaches} = this.state
         return (
-            <div>
-                <h2>All GeoCaches</h2>
-                <table className="geo-table">
-                    <thead>
-                        <tr>
-                            <th>Coordinates</th>
-                            <th>Description</th>
-                            <th>Gifter</th>
-                            <th>Reviewer</th>
-                            <th>Finder</th>
-                            <th>Found</th>
-                            <th>TimeLimit</th>
-                        </tr>
-                    </thead>
-                    <tbody>    
-                    {
-                        allGeoCaches ? 
-                        allGeoCaches.map( gc => { 
-                            return(
-                                <tr className="geo-row">
-                                    <td> {gc.coordinates[0]}, {gc.coordinates[1]}  </td>
-                                    <td>| {gc.description}  </td>
-                                    <td>| {gc.gifter}  </td>
-                                    <td>| {gc.reviewer? gc.reviewer: "null"}  </td>
-                                    <td>| {gc.finder}  </td>
-                                    <td>| {gc.found.toString()}  </td>
-                                    <td>| {gc.timeLimit}</td>
-                                </tr>
-                            )
-                        } ) :
-                        "No GeoCaches to Show"
-                    }
-                    </tbody>
-                </table>
-            </div>
+            <GeoCachesTable geoCaches={allGeoCaches} />
         )
     }
 }
